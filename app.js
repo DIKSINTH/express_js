b3e5c203d6c77d1d7e1ce2f15dd420bf3f66d646
DIKSINTH <diksinth1@gmail.com>
2025-09-15 15:44:03 +0530

    new

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require("express");
-// const http = require("http");
 const app = express();
-app.use((req, res, next) => {
-  console.log("Middleware 1");
-  next();
+app.use("/second", (req, res, next) => {
+  res.send("<h1>Second Page</h1>");
+  console.log("Second Middleware");
 });
-app.use((req, res, next) => {
-  console.log("Middleware 2");
-  next();
+app.use("/", (req, res, next) => {
+  res.send("<h1>First Page</h1>");
+  console.log("First Middleware");
 });
-app.use((req, res, next) => {
-  console.log("Middleware 3");
-  next();
-});
-app.use((req, res, next) => {
-  console.log("Middleware 4");
-});
-// const server = http.createServer(app);
-// server.listen(3000);
 app.listen(3000);
